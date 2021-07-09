a71c51223082cea50f62e22fdd6063c016d84186
banshan <dwdcth@163.com>
2021-07-09 10:51:53 +0800

    Update main.go
    代码对齐

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-    "time"
+	"time"
 
 	. "github.com/Monibuca/engine/v3"
 	. "github.com/Monibuca/utils/v3"
@@ -58,8 +58,8 @@ func run() {
 	}
 	config.createTime = time.Now()
 	if config.Formatter == "" {
-        config.Formatter = "2006-01-02T15"
-    }
+        	config.Formatter = "2006-01-02T15"
+	}
 	err := os.MkdirAll(config.Path, 0777)
 	config.file, err = os.OpenFile(filepath.Join(config.Path, fmt.Sprintf("%s.log", config.createTime.Format(config.Formatter))), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
 	if err == nil {
