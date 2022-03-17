53baaa5b5538729731899b23bf960e284e87a09c
dexter <178529795@qq.com>
2022-03-17 17:01:33 +0800

    v4 firstcommit

--- main.go
+++ main.go
@@ -5,84 +5,80 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"time"
 
-	. "github.com/Monibuca/engine/v3"
-	. "github.com/Monibuca/utils/v3"
+	. "m7s.live/engine/v4"
+	"m7s.live/engine/v4/log"
+	. "m7s.live/engine/v4/util"
 )
-
-var config LogRotate
 
 type FileInfo struct {
 	Name string
 	Size int64
 }
-type LogRotate struct {
+type LogRotateConfig struct {
 	Path        string
 	Size        int64
 	Days        int
+	Formatter   string
 	file        *os.File
 	currentSize int64
 	createTime  time.Time
 	hours       float64
 	splitFunc   func() bool
-	Formatter   string
 }
 
-func init() {
-	pc := PluginConfig{
-		Name:   "LogRotate",
-		Config: &config,
-	}
-	pc.Install(run)
-}
-func run() {
-	http.HandleFunc("/api/logrotate/tail", watchLogs)
-	http.HandleFunc("/api/logrotate/find", findLog)
-	http.HandleFunc("/api/logrotate/list", listLogFiles)
-	http.HandleFunc("/api/logrotate/open", openLog)
-	http.HandleFunc("/api/logrotate/download", download)
-	if config.Size > 0 {
-		config.splitFunc = config.splitBySize
-	} else {
-		if config.Days == 0 {
-			config.Days = 1
+var plugin = InstallPlugin(&LogRotateConfig{
+	Path:      "./logs",
+	Days:      1,
+	Formatter: "2006-01-02T15",
+})
+
+func (config *LogRotateConfig) OnEvent(event any) {
+	switch event.(type) {
+	case FirstConfig:
+		if config.Size > 0 {
+			config.splitFunc = config.splitBySize
+		} else {
+			if config.Days == 0 {
+				config.Days = 1
+			}
+			config.hours = float64(config.Days) * 24
+			config.splitFunc = config.splitByTime
 		}
-		config.hours = float64(config.Days) * 24
-		config.splitFunc = config.splitByTime
-	}
-	config.createTime = time.Now()
-	if config.Formatter == "" {
-		config.Formatter = "2006-01-02T15"
-	}
-	err := os.MkdirAll(config.Path, 0777)
-	config.file, err = os.OpenFile(filepath.Join(config.Path, fmt.Sprintf("%s.log", config.createTime.Format(config.Formatter))), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
-	if err == nil {
-		stat, _ := config.file.Stat()
-		config.currentSize = stat.Size()
-		AddWriter(&config)
-	} else {
-		log.Println(err)
+		config.createTime = time.Now()
+		if config.Formatter == "" {
+			config.Formatter = "2006-01-02T15"
+		}
+		err := os.MkdirAll(config.Path, 0655)
+		config.file, err = os.OpenFile(filepath.Join(config.Path, fmt.Sprintf("%s.log", config.createTime.Format(config.Formatter))), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+		if err == nil {
+			stat, _ := config.file.Stat()
+			config.currentSize = stat.Size()
+			log.AddWriter(config)
+		} else {
+			log.Error(err)
+		}
 	}
 }
-func (l *LogRotate) splitBySize() bool {
+
+func (l *LogRotateConfig) splitBySize() bool {
 	return l.currentSize >= l.Size
 }
-func (l *LogRotate) splitByTime() bool {
+func (l *LogRotateConfig) splitByTime() bool {
 	return time.Since(l.createTime).Hours() > l.hours
 }
-func (l *LogRotate) Write(data []byte) (n int, err error) {
+func (l *LogRotateConfig) Write(data []byte) (n int, err error) {
 	n, err = l.file.Write(data)
 	l.currentSize += int64(n)
 	if err == nil {
 		if l.splitFunc() {
 			l.createTime = time.Now()
-			if file, err := os.OpenFile(filepath.Join(l.Path, fmt.Sprintf("%s.log", l.createTime.Format(config.Formatter))), os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0666); err == nil {
+			if file, err := os.OpenFile(filepath.Join(l.Path, fmt.Sprintf("%s.log", l.createTime.Format(l.Formatter))), os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0666); err == nil {
 				l.file = file
 				l.currentSize = 0
 			}
@@ -90,20 +86,21 @@ func (l *LogRotate) Write(data []byte) (n int, err error) {
 	}
 	return
 }
-func watchLogs(w http.ResponseWriter, r *http.Request) {
-	AddWriter(NewSSE(w, r.Context()))
+func (l *LogRotateConfig) API_tail(w http.ResponseWriter, r *http.Request) {
+	writer := NewSSE(w, r.Context())
+	log.AddWriter(writer)
 	<-r.Context().Done()
+	log.DeleteWriter(writer)
 }
-func findLog(w http.ResponseWriter, r *http.Request) {
-	cmd := exec.Command("grep", fmt.Sprintf("\"%s\"", r.URL.Query().Get("query")), config.Path)
+func (l *LogRotateConfig) API_find(w http.ResponseWriter, r *http.Request) {
+	cmd := exec.Command("grep", fmt.Sprintf("\"%s\"", r.URL.Query().Get("query")), l.Path)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	cmd.Run()
 	w.Write([]byte(out.String()))
 }
-func listLogFiles(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	dir, err := os.Open(config.Path)
+func (l *LogRotateConfig) API_list(w http.ResponseWriter, r *http.Request) {
+	dir, err := os.Open(l.Path)
 	defer func() {
 		if err != nil {
 			w.Write([]byte(err.Error()))
@@ -130,9 +127,9 @@ func listLogFiles(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Write(bytes)
 }
-func download(w http.ResponseWriter, r *http.Request) {
+func (l *LogRotateConfig) API_download(w http.ResponseWriter, r *http.Request) {
 	filename := r.URL.Query().Get("file")
-	file, err := os.Open(filepath.Join(config.Path, filename))
+	file, err := os.Open(filepath.Join(l.Path, filename))
 	defer func() {
 		if err != nil {
 			w.Write([]byte(err.Error()))
@@ -147,9 +144,9 @@ func download(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
-func openLog(w http.ResponseWriter, r *http.Request) {
+func (l *LogRotateConfig) API_open(w http.ResponseWriter, r *http.Request) {
 	filename := r.URL.Query().Get("file")
-	file, err := os.Open(filepath.Join(config.Path, filename))
+	file, err := os.Open(filepath.Join(l.Path, filename))
 	defer func() {
 		if err != nil {
 			w.Write([]byte(err.Error()))
